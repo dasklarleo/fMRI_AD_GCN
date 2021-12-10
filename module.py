ea47b7f882a281c1a58de38c701854e5dbe92e19
dasklarleo <2353584083@qq.com>
2021-12-10 20:54:38 +0800

    finish the model without testing

--- module.py
+++ module.py
@@ -1,9 +1,10 @@
 import torch
 import random
+from torch import serialization
 import torch.nn
 import numpy as np
 import torch.nn.functional as F
-from torch.nn.modules import padding
+from torch.nn.modules import padding, transformer
 from torch.nn.modules.activation import ReLU
 
 # Method
@@ -35,6 +36,7 @@ class Transofrmer(torch.nn.Module):
 
         return relation
 #In the squeeze module what squeeze algorithm should I use?
+
 class SE_Block(torch.nn.module):
     def __init__(self,SE_Parameter,heads):
         super(SE_Block,self).__init__()
@@ -51,7 +53,8 @@ class SE_Block(torch.nn.module):
         output=F.sigmoid(self.W_2(bottle_neck))
         return output
 
-# The first time we call the GCN module, we need to make the features from [Batchsize,ROIs,features] to [Batchsize,heads,ROIs,features] by repeating it heads times
+# The first time we call the GCN module, we need to make the features 
+# from [Batchsize,ROIs,features] to [Batchsize,heads,ROIs,features] by repeating it heads times
 class GCN(torch.nn.module):
     def __init__(self,features,dilated_parameter_k):
         super(GCN,self).__init__()
@@ -88,19 +91,92 @@ class GCN(torch.nn.module):
         #ResNet
         return output1+features
 
-#fusion the infomations in different heads
-class fusion(torch.nn.modules):
-    def __init__(self,heads,batch_size) -> None:
-        super().__init__(fusion,self)
+'''
+fusion the infomations in different heads
+    - inputs: [batch_size,heads,ROIs,features]
+    - outputs: [batch_size,1,ROIs,features]
+'''
+class Fusion(torch.nn.modules):
+    def __init__(self,heads) -> None:
+        super().__init__(Fusion,self)
         self.heads=heads
         self.conv=torch.nn.Conv2d(in_channels=heads,out_channels=1,stride=1,padding=0)
     def forward(self,output):
         return (self.conv(output))
+'''
+Output the classfication informations
+    - inputs: [batch_size,ROIs,features]
+    - outputs: [batch_size,predict_labels]
+'''
 class MLP(torch.nn.modules):
-    def __init__(self):
-        super().__init__()
-        pass
-    def forward(self):
-        pass
+    def __init__(self,features):
+        super(MLP,self).__init__()
+        self.MLP1=torch.nn.Linear(features,256)
+        self.MLP2=torch.nn.Linear(256,128)
+        self.MLP3=torch.nn.Linear(128,64)
+        self.MLP4=torch.nn.Linear(64,5)
+    def forward(self,fusion_output):
+        output1=self.MLP1(fusion_output)
+        output1=F.relu(output1)
+        output2=self.MLP2(output1)
+        output2=F.relu(output2)
+        output3=self.MLP3(output2)
+        output3=F.relu(output3)
+        predict_labels=F.relu(self.MLP4(output3))
+        return predict_labels
+
+class whole_network(torch.nn.modules):
+    def __init__(self,batch_size,ROI_nums,heads,BOLD_nums,q,
+    SE_parameter,
+    feature_nums,dilated_parameter_k):
+        super(whole_network,self).__init__()
+        self.heads=heads
+        self.batch_size=batch_size
+        self.feature_nums=feature_nums
+        self.ROI_nums=ROI_nums
+        self.BOLD_nums=BOLD_nums#BOLD信号采样的个数
+        self.transofrmer=Transofrmer(batch_size,ROI_nums,heads,BOLD_nums,q)
+        self.se=SE_Block(SE_parameter,heads)
+        self.gcn_layer1=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer2=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer3=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer4=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer5=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer6=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer7=GCN(feature_nums,dilated_parameter_k)
+        self.gcn_layer8=GCN(feature_nums,dilated_parameter_k)
+        self.fusion=Fusion(heads)
+        self.mlp=MLP(feature_nums)
+    def forwara(self,BOLD_signals):        #BOLD_signals[batch_size,ROIs,features]
+
+        relation=self.transofrmer(BOLD_signals)        #relation[batch_size,heads,ROIs,ROIs]
+
+        relation=self.se(relation)        #relation[batch_size,heads,ROIs,ROIs]
+
+        BOLD_signals=BOLD_signals.view(self.batch_size,1,self.ROI_nums,self.BOLD_nums)        #BOLD_signals[batch_size,1,ROI_nums,BOLD_nums]
+
+        BOLD_signals=torch.cat((BOLD_signals,BOLD_signals,BOLD_signals,BOLD_signals,
+        BOLD_signals,BOLD_signals,BOLD_signals,BOLD_signals,),dim=1)        #BOLD_signals[batch_size,8(heads),ROI_nums,BOLD_nums]
+
+        GCN_output=self.gcn_layer1(BOLD_signals) 
+        GCN_output=self.gcn_layer2(GCN_output)       
+        GCN_output=self.gcn_layer3(GCN_output)       
+        GCN_output=self.gcn_layer4(GCN_output)       
+        GCN_output=self.gcn_layer5(GCN_output)       
+        GCN_output=self.gcn_layer6(GCN_output)       
+        GCN_output=self.gcn_layer7(GCN_output)       
+        GCN_output=self.gcn_layer8(GCN_output) #GCN_output:[batch_size,heads,ROIs,features]
+
+        fusion_output=self.fusion(GCN_output) #fusion_output:[batch_size,1,ROIs,features]
+        fusion_output=fusion_output.view(self.batch_size,self.ROI_nums,self.BOLD_nums)
+
+        predict_labels=self.mlp(fusion_output)# predict_labels:[batch_size,labels]
+
+        return predict_labels
+    
+
+
+
+        
 
 
