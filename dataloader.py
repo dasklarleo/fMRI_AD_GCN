92381d41d445f9f84f854426506e40c593c18279
dasklarleo <2353584083@qq.com>
2021-12-12 16:06:46 +0800

    conding

--- dataloader.py
+++ dataloader.py
@@ -1,18 +1,18 @@
-#dataloader: load the data from the matlab file
+# dataloader: load the data from the matlab file
 from numpy.core.fromnumeric import shape
 import torch
-from torch.utils.data import Dataset,DataLoader,TensorDataset
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torch.utils.data.sampler import SubsetRandomSampler
 import numpy as np
 import os
 import scipy.io as scio
 import math
-batch_size=16
 
-def load_data(data_folder_path):
-    data=[]
-    labels=[]
-    files=os.listdir(data_folder_path)
+
+def load_data(data_folder_path, batch_size):
+    data = []
+    labels = []
+    files = os.listdir(data_folder_path)
     for file in files:
         if 'DFC' in file:
             continue
@@ -31,34 +31,37 @@ def load_data(data_folder_path):
         ROI_BOLDs = scio.loadmat(data_folder_path+'/'+file)['ROI_ts']
         data.append(ROI_BOLDs)
 
-    data=np.array(data)
-    labels=np.array(labels)
+    data = np.array(data)
+    labels = np.array(labels)
     # randomly shuffle the data
-    state=np.random.get_state()
+    state = np.random.get_state()
     np.random.shuffle(data)
     np.random.set_state(state)
     np.random.shuffle(labels)
     # cut the data into training data and valiadation data
-    data_len=data.shape[0]
-    train_len=math.ceil(data_len*0.8)
-    train_data=data[0:train_len]
-    valiad_data=data_len[train_len,data_len]
-    train_labels=labels[0:train_len]
-    valiad_labels=labels[train_len,data_len]
-    #put the data in the dataloader
-    train_data=torch.from_numpy(train_data)
-    train_labels=torch.from_numpy(train_labels)
-    valiad_data=torch.from_numpy(valiad_data)
-    valiad_labels=torch.from_numpy(valiad_labels)
-    train_dataset=Dataset.TensorDataset(train_data,train_labels)
-    valid_dataset=Dataset.TensorDataset(valiad_data,valiad_labels)
-    #dataloader
-    train_loader=DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True)
-    valid_loader=DataLoader(dataset=valid_dataset,batch_size=batch_size,shuffle=True)
-    return train_loader,valid_loader
+    data_len = data.shape[0]
+    train_len = math.ceil(data_len*0.8)
+    train_data = data[0:train_len]
+    valiad_data = data[train_len:data_len]
+    train_labels = labels[0:train_len]
+    valiad_labels = labels[train_len:data_len]
+    # put the data in the dataloader
+    train_data = torch.from_numpy(train_data)
+    train_labels = torch.from_numpy(train_labels)
+    valiad_data = torch.from_numpy(valiad_data)
+    valiad_labels = torch.from_numpy(valiad_labels)
+    train_dataset = TensorDataset(train_data, train_labels)
+    valid_dataset = TensorDataset(valiad_data, valiad_labels)
+    # dataloader
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(dataset=valid_dataset,
+                              batch_size=batch_size, shuffle=True)
+    return train_loader, valid_loader
 
-if __name__=='__main__':
-    data,labels=load_data("/home/leosher/data/pet/fMRI_BOLD/par100")
+
+if __name__ == '__main__':
+    data, labels = load_data("/home/leosher/data/pet/fMRI_BOLD/par100")
     print(len(data))
-    print(data.shape)
-    print(labels.shape)
+    print(data)
+    print(labels)
--- train.py
+++ train.py
@@ -2,6 +2,11 @@
 In this file the hyper-paramters will be set
 The training loops would also be set
 '''
-def train_fn():
+import dataloader
+import model
+if __name__ == '__main__':
+    pass
+    dataloader("/home/leosher/data/pet/fMRI_BOLD/par100", 16)
+    net = model.whole_network(batch_size=16, ROI_nums=130, heads=8, BOLD_nums=1000, q=128, SE_parameter=4,
+                              feature_nums=1000, dilated_parameter_k=2)
     
-
